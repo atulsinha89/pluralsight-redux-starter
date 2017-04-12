1300be5f734f9853a889b38c3d40645ce19118bf
Atul Sinha <atul.sinha@pharmeasy.in>
2017-04-12 18:57:47 +0530

    added error handling for save form, rectified console warnings

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -2,15 +2,15 @@ import React, { PropTypes } from 'react';
 import TextInput from '../common/TextInput';
 import SelectInput from '../common/SelectInput';
 
-const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+const CourseForm = ({course, allAuthors, onSave, onChange, saving, errors}) => {
   return (
     <form>
       <h1>Manage Course</h1>
       <TextInput name="title" label="Title" value={course.title} onChange={onChange} error={errors.title} />
-      <SelectInput name="authorId" title="Author" value={course.authorId} defaultOption="Select Author" options={allAuthors} onChange={onChange} error={errors.authorId}/>
-      <TextInput name="category" title="Category" value={course.category} onChange={onChange} error={errors.category} />
-      <TextInput name="length" title="Length" value={course.length} onChange={onChange} error={errors.length} />
-      <input type="submit" disabled={loading} value={loading? 'Saving...':'Save'} className="btn btn-primary" onClick={onSave}/>
+      <SelectInput name="authorId" label="Author" value={course.authorId} defaultOption="Select Author" options={allAuthors} onChange={onChange} error={errors.authorId}/>
+      <TextInput name="category" label="Category" value={course.category} onChange={onChange} error={errors.category} />
+      <TextInput name="length" label="Length" value={course.length} onChange={onChange} error={errors.length} />
+      <input type="submit" disabled={saving} value={saving? 'Saving...':'Save'} className="btn btn-primary" onClick={onSave}/>
     </form>
   );
 };
@@ -20,7 +20,7 @@ CourseForm.propTypes = {
   allAuthors: PropTypes.array,
   onSave: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
-  loading: PropTypes.bool,
+  saving: PropTypes.bool,
   errors: PropTypes.object
 };
 
